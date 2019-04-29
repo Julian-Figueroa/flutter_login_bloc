83f4f306f6f76e85e2011cf26f29b22474e4ad68
Julian Figueroa <julianfigueroatorres@gmail.com>
2019-04-28 19:04:54 -0500

    first commit with Bloc approach without using RxDart yet

---
+++ README.md
@@ -0,0 +1,11 @@
+# login_bloc
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+Dart - Flutter App
+
+Form with StreamBuilders using the Bloc (Business Logic Component) approach
---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
+import '../blocs/provider.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final bloc = Provider.of(context);
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 25.0),
+          ),
+          emailField(bloc),
+          passwordField(bloc),
+          Container(
+            margin: EdgeInsets.only(top: 25.0),
+          ),
+          submitButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget emailField(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.email,
+      builder: (BuildContext context, snapshot) {
+        return TextField(
+          onChanged: bloc.changeEmail,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: 'you@example.com',
+            labelText: 'Email Address',
+            errorText: snapshot.error,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget passwordField(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.password,
+      builder: (BuildContext context, snapshot) {
+        return TextField(
+          onChanged: bloc.changePassword,
+          keyboardType: TextInputType.text,
+          obscureText: true,
+          decoration: InputDecoration(
+            hintText: 'Password',
+            labelText: 'Password',
+            errorText: snapshot.error,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text('Login'),
+      color: Colors.blue,
+      onPressed: () {},
+    );
+  }
+}
